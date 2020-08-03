b8c6990df939a99c299becc6c049eda37bf92994
TheTallPaul <paul.lantow@gmail.com>
2020-08-02 17:33:22 -0700

    Added tests for the Google Maps LatLng type
    The tests will fail since we have not added a check for the maps package (yay TDD!).
    
    googlemaps.github.io/maps

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module haversine
 
 go 1.14
 
-require google.golang.org/genproto v0.0.0-20200731012542-8145dea6a485
+require (
+	google.golang.org/genproto v0.0.0-20200731012542-8145dea6a485
+	googlemaps.github.io/maps v1.2.1
+)
--- haversine_test.go
+++ haversine_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"google.golang.org/genproto/googleapis/type/latlng"
+	"googlemaps.github.io/maps"
 )
 
 var zeroPoint = latlng.LatLng{Latitude: 0.0, Longitude: 0.0}
@@ -31,14 +32,40 @@ var haversineDistanceTestCases = []struct {
 	},
 }
 
+var zeroPointMaps = maps.LatLng{Lat: 0.0, Lng: 0.0}
+var onePointMaps = maps.LatLng{Lat: 1.0, Lng: -1.0}
+var broadwayYamhillMaps = maps.LatLng{Lat: 45.518673, Lng: -122.679996}
+var pioneerCourthouseMaps = maps.LatLng{
+	Lat:  45.51867299282918,
+	Lng: -122.67871393198214,
+}
+
+var haversineDistanceTestCasesMaps = []struct {
+	pointA           *maps.LatLng
+	pointB           *maps.LatLng
+	expectedDistance float64
+}{
+	{
+		&zeroPointMaps,
+		&onePointMaps,
+		157249.38127194397,
+	},
+	{
+		&broadwayYamhillMaps,
+		&pioneerCourthouseMaps,
+		99.88810211965017,
+	},
+}
+
 func TestHaversineDistance(t *testing.T) {
 	for _, input := range haversineDistanceTestCases {
 		distance := HaversineDistance(input.pointA, input.pointB)
 
 		if distance != input.expectedDistance {
 			t.Errorf(
-				"FAIL: Want distance from (%v, %v) to (%v, %v) to be: "+
-					"%v me but we got %v m",
+				"FAIL: Want distance from (%v, %v) to (%v, %v"+
+					") to be: %v me but we got %v m ("+
+					"latlng package)",
 				input.pointA.Latitude,
 				input.pointA.Longitude,
 				input.pointB.Latitude,
@@ -48,4 +75,23 @@ func TestHaversineDistance(t *testing.T) {
 			)
 		}
 	}
+
+	for _, input := range haversineDistanceTestCasesMaps {
+		distance := HaversineDistance(input.pointA, input.pointB)
+
+		if distance != input.expectedDistance {
+			t.Errorf(
+				"FAIL: Want distance from (%v, %v) to (%v, %v"+
+					") to be: %v me but we got %v m ("+
+					"maps package)",
+				input.pointA.Lat,
+				input.pointA.Lng,
+				input.pointB.Lat,
+				input.pointB.Lng,
+				input.expectedDistance,
+				distance,
+			)
+		}
+	}
+
 }
